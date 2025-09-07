72256291e6e9109c80804a06815999dce3b8addf
Olivia <omaoloni@gmail.com>
2025-09-07 13:42:15 -0400

    Add additional docstrings and fix formatting

--- rank.py
+++ rank.py
@@ -1,11 +1,13 @@
 from player import Player
 
 
-def get_sorted_player_list(players: list[tuple[str, str]], sort_descending: bool) -> list[tuple[str, str]]:
-    """ 
+def get_sorted_player_list(
+    players: list[tuple[str, str]], sort_descending: bool
+) -> list[tuple[str, str]]:
+    """
     Return a list of players sorted by score with flexibility on asc or desc order.
 
-    :param players is a list of tuple in the format, ('player_name', 'score')
+    :param players: is a list of tuple in the format, ('player_name', 'score')
 
     :Example:
     >>> get_sorted_player_list([('Olivia', 8), ('Buffy', 10), ('Hermione', '9')], True)
@@ -19,7 +21,7 @@ def get_ranking(players: list[tuple[str, str]]) -> dict[str, str]:
     """
     Return a dict of {'player_name' : 'ranking'}
 
-    :param players is a list of tuple in the format, ('player_name', 'score')
+    :param players: is a list of tuple in the format, ('player_name', 'score')
 
     :Example:
     >>> get_ranking([('Olivia', 8), ('Buffy', 10), ('Hermione', '9')])
@@ -27,19 +29,24 @@ def get_ranking(players: list[tuple[str, str]]) -> dict[str, str]:
     """
     sorted_players = get_sorted_player_list(players, True)
 
-    player_rankings = { player[0] : rank for rank, player in enumerate(sorted_players, start=1)}
+    player_rankings = {
+        player[0]: rank for rank, player in enumerate(sorted_players, start=1)
+    }
 
     print(player_rankings)
     return player_rankings
 
 
-def get_power_ranking(existing_player_standings: dict[str, str], current_player_scores: list[tuple[str, str]]) -> list[Player]:
+def get_power_ranking(
+    existing_player_standings: dict[str, str],
+    current_player_scores: list[tuple[str, str]],
+) -> list[Player]:
     """
     Return a sorted list (desc) of `Players` based on their current score.
     Calculate their `power_ranking` as the difference between their standing in the previous episode and current episode.
 
-    :param existing_player_standings is a representation of the standings as of the previous episode
-    :param current_player_scores are the updated scores for each player as of the current episode
+    :param existing_player_standings: is a representation of the standings as of the previous episode
+    :param current_player_scores: contains the updated scores for each player as of the current episode
 
     :Example:
     >>> get_power_ranking({'Buffy': '1', 'Hermione': '2', 'Olivia': '3'}, [('Olivia', 25), ('Buffy', 28), ('Hermione', '22')])
--- workbook.py
+++ workbook.py
@@ -10,17 +10,19 @@ def get_player_scores(episode_no: int, working_directory: str) -> list[tuple[str
 
     The expected workbook format is that player names are on row 1 and final scores are on row 22.
 
-    :param episode_no is the episode number for which to retrieve the player names and scores
-    :param working_directory is the directory containing the Excel workbook to read
+    :param episode_no: is the episode number for which to retrieve the player names and scores
+    :param working_directory: is the directory containing the Excel workbook to read
 
     :Example:
     >>> get_player_scores(3, "C:/path/to/directory")
     [('Olivia', 8), ('Buffy', 10), ('Hermione', '9')]
     """
     if episode_no is None or (episode_no < 1 or episode_no > 13):
-        return "" # TODO raise error; also add special handling for ep1 as it should be calculated differently
+        return ""  # TODO raise error; also add special handling for ep1 as it should be calculated differently
 
-    workbook = openpyxl.load_workbook(get_workbook_filepath(working_directory), data_only=True, read_only=True)
+    workbook = openpyxl.load_workbook(
+        get_workbook_filepath(working_directory), data_only=True, read_only=True
+    )
     worksheet = workbook[constants.WORKBOOK_SHEET_NAME_PREFIX + str(episode_no)]
 
     names = get_row_data(worksheet, 1)
@@ -32,7 +34,7 @@ def get_player_scores(episode_no: int, working_directory: str) -> list[tuple[str
 
 
 def get_workbook_filepath(working_directory: str) -> str:
-    """ Return the full path to the Excel workbook. """
+    """Return the full path to the Excel workbook."""
     return os.path.join(working_directory, constants.WORKBOOK_FILE_NAME)
 
 
@@ -41,12 +43,11 @@ def get_row_data(worksheet: object, row_index: int) -> list:
     Return a list of non-null values from a row in the worksheet.
     The first column is omitted as it is the label for the row.
 
-    :param worksheet is a openpyxl Worksheet object
-    :param row_index is the row of the `worksheet` to process
+    :param worksheet: is a openpyxl Worksheet object
+    :param row_index: is the row of the `worksheet` to process
 
     :Example:
     >>> get_row_data(worksheet_object, 1)
     ['Olivia', 'Buffy', 'Hermione']
     """
     return [cell.value for cell in worksheet[row_index][1:] if cell.value is not None]
-
